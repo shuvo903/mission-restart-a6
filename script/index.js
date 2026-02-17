9515f9cae188f3a6433c3fa07102fb30f1a394e7
shuvo903 <saifulislamshuvoa123@gmail.com>
2026-02-18 02:45:36 +0600

    added our product page and api data

--- script/index.js
+++ script/index.js
@@ -1,1 +1,8 @@
-console.log("This is js");
+const url = "https://fakestoreapi.com/products"
+
+const product = () =>{
+    const data = fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    
+}
