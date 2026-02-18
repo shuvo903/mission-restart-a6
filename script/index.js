3b8aebec45c66e24a51d45b8aa9c7a4b4e08bc24
shuvo903 <saifulislamshuvoa123@gmail.com>
2026-02-19 02:32:51 +0600

    added our product section category buttons

--- script/index.js
+++ script/index.js
@@ -1,8 +1,44 @@
-const url = "https://fakestoreapi.com/products"
-
-const product = () =>{
-    const data = fetch("https://fakestoreapi.com/products")
+const loadProduct = () => {
+  fetch("https://fakestoreapi.com/products/categories")
     .then((res) => res.json())
-    .then((data) => console.log(data))
-    
+    .then((json) => displayProduct(json));
+};
+const loadProducts = (category) =>  {
+
+    const url = `https://fakestoreapi.com/products/category/${category}`
+    console.log(url);
 }
+
+
+const displayProduct = (products) => {
+  const productBtnContainer = document.getElementById("product-btn-container");
+
+  productBtnContainer.innerHTML = "";
+
+  const allBtn = document.createElement("button");
+  allBtn.innerHTML = `
+
+  
+        
+<button " class="btn btn-outline  btn-primary rounded-full mb-10">All</button>
+
+
+`;
+
+  productBtnContainer.append(allBtn);
+
+  for (let product of products) {
+    const btnDiv = document.createElement("div");
+
+    btnDiv.innerHTML = `  
+    
+<button onclick="loadProducts(${product.category})" class="btn btn-outline  btn-primary rounded-full mb-10"> ${product}</button>
+
+
+`;
+
+    productBtnContainer.append(btnDiv);
+  }
+};
+
+loadProduct();
